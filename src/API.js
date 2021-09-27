a8ff4ba0aed24eede709c6282c2c52ec60835b5f
Scorpion197 <doudou.gaw@gmail.com>
2021-09-27 19:20:46 +0200

    Added config and env files

---
+++ src/API.js
@@ -0,0 +1,23 @@
+import {
+    SEARCH_BASE_URL,
+    POPULAR_BASE_URL,
+    API_URL,
+    API_KEY,
+    REQUEST_TOKEN_URL,
+    LOGIN_URL,
+    SESSION_ID_URL,
+  } from './config';
+  
+  const defaultConfig = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+
+const API = {
+
+
+    
+}
